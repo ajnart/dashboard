5270c3176b4491be0e462712238237d7b59385f3
Aj - Thomas <thomas.camlong@epitech.eu>
2021-11-15 17:10:38 +0100

    Update login UI and components

--- front/pages/login.js
+++ front/pages/login.js
@@ -1,14 +1,24 @@
 import { Box, Spacer, Center, ButtonGroup, Button, InputGroup, Input, InputRightElement } from "@chakra-ui/react"
-import React, { useState } from 'react';
+import React, { useState, setValue } from 'react';
 
 export default function Home() {
 	const [show, setShow] = useState(false)
+	const [login, password] = useState('')
+	const changePass = (e) => { password = e.target.value }
+	const changeUser = (e) => { login = e.target.value }
+
 	const handleClick = () => setShow(!show)
+	const submit = () => {
+		//TOOD: Add backend request to login
+		console.log(`Attemping to login with ${login}:${password}`)
+	}
 
 	return (
 		<>
 		<Box padding="40" paddingBottom="10">
-				<Input placeholder="Username" />
+			<Input 
+				placeholder="Username" />
+			<Spacer my="2" />
 			<InputGroup>
 				<Input
 					type={show ? "text" : "password"}
