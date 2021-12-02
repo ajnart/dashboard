c041836543cddf00f71eb14f3dfd5fdc1890d77b
Nathan GD <nathan.gardet-derc@protonmail.ch>
2021-12-02 14:49:18 +0100

    [ADD] complete page

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,14 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
+import { CookiesProvider} from 'react-cookie'
 // import { AnimatePresence } from 'framer-motion'
 // import theme from '../libs/theme'
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <ChakraProvider /*theme={theme}*/>
-      <Layout router={router}>
+      <CookiesProvider>
+      <Layout router={router}> 
           <Component {...pageProps} />
       </Layout>
+    </CookiesProvider>
     </ChakraProvider>
   )
 }
