5bb6a635b9f103984047f2ae87dcce99e47b0f27
Baptiste ARRECOT <baptiste.arrecot64@gmail.com>
2021-12-02 11:59:43 +0100

    [FIX] fixed the service infos to put which widgets correspond to each services + minor fixes

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const sqlite3 = require('sqlite3');
 
-const db = new sqlite3.Database('./Logins.db', (err) => {
+const db = new sqlite3.Database('./Dashboard.db', (err) => {
     if (err) throw err;
     console.log("Connected to mysql db!");
     db.run('CREATE TABLE IF NOT EXISTS USERS_TABLE(userID, username, email, password, Token, Refreshtoken)');
-    db.run('CREATE TABLE IF NOT EXISTS SERVICES_TABLE(userID, name, position, serviceID)');
+    db.run('CREATE TABLE IF NOT EXISTS SERVICES_TABLE(userID, name, position, widgetsIDs, serviceID)');
     db.run('CREATE TABLE IF NOT EXISTS WIDGET_TABLE(userID, serviceName, name, description, params, widgetID)'); 
 });
 
--- backend/routes/widget.js
+++ backend/routes/widget.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const { randomBytes } = require('crypto');
 const db = require('../db');
 
-router.get('/fetch', (req, res) => {       
-    const { token, name, serviceName } = req.body;
+router.get('/fetchAll', (req, res) => {       
+    const { token, serviceName } = req.body;
 
     db.all('SELECT * FROM USERS_TABLE WHERE Token = ?', token, (err, profile) => {
         if (err) { 
             throw err;
         }
         if (profile[0]) {
-            db.all('SELECT * FROM WIDGET_TABLE WHERE userID = ? and name = ? and serviceName = ?', [profile[0].userID, name, serviceName], (err, data) => {
+            db.all('SELECT * FROM WIDGET_TABLE WHERE userID = ? and serviceName = ?', [profile[0].userID, serviceName], (err, data) => {
                 if (err) { 
                     throw err;
                 }
                 if (data[0]) {
-                    res.status(202).send(data[0]); 
+                    res.status(202).send(data); 
                 } else {
                     res.status(404).send({message: 'Service not found'});
                 }
@@ -56,6 +56,8 @@ router.put('/edit', (req, res) => {
     const {token, name, description, serviceName, params}= req.body;
     const sqlInsert = "INSERT INTO WIDGET_TABLE(userID, name, description, serviceName, params, widgetID) VALUES(?, ?, ?, ?, ?, ?)";
     const sqlUpdate = "UPDATE WIDGET_TABLE SET description = ? and serviceName = ? and params = ?  where name = ? and userID = ?";
+    const serviceInsert = "UPDATE SERVICES_TABLE SET widgetsIDs = ? WHERE serviceID = ?";
+    const serviceSelect = "SELECT * FROM SERVICES_TABLE WHERE name = ? and userID = ?";
 
 
     db.all('SELECT * FROM USERS_TABLE WHERE Token = ?', token, (err, profile) => {
@@ -63,24 +65,51 @@ router.put('/edit', (req, res) => {
             throw err;
         }
         if (profile[0]) {
-            db.all('SELECT * FROM WIDGET_TABLE WHERE name = ?' , [name], (err, data) => {
-                console.log(data);
-                if (data[0]) {
-                    console.log(data[0]);
-                    db.run(sqlUpdate, [description, serviceName, JSON.stringify(params), name, data[0].userID], (err) => {
-                        if (err) { 
-                            throw err; 
-                        }
-                        res.status(201).send({message: 'Widget edited successfully'});
-                    });
-                } else {
-                    db.run(sqlInsert, [profile[0].userID, name, description, serviceName, JSON.stringify(params), widgetID], (err) => {
-                        if (err) { 
-                            throw err; 
-                        }
-                        res.status(201).send({message: 'Widget set successfully'});
-                    });
+            db.all('SELECT * FROM WIDGET_TABLE WHERE name = ? and userID = ?' , [name, profile[0].userID], (err, data) => {
+                if (err) { 
+                    throw err; 
                 }
+                db.all(serviceSelect, [serviceName, profile[0].userID], (err, service) => {
+                    if (err) { 
+                        throw err; 
+                    }     
+                    if (data[0] && service[0]) {
+                        db.run(sqlUpdate, [description, serviceName, JSON.stringify(params), name, data[0].userID], (err) => {
+                            if (err) {
+                                throw err; 
+                            }
+                            new Promise((resolve, reject) => {
+                                if (service[0].widgetsName) {
+                                    service[0].widgetsName[service[0].widgetsName.length] = widgetID;
+                                    resolve(service[0].widgetsName);
+                                } else {
+                                    service[0].widgetsName = [widgetID];
+                                    resolve(service[0].widgetsName);
+                                }
+                            }).then ((widgetsName) => {
+                                db.run(serviceInsert, [JSON.stringify(widgetsName), service[0].serviceID], (err) => {
+                                    if (err) { throw err; }
+                                    res.status(201).send({message: 'Widget edited successfully'});
+                                });
+                            })
+                        });
+                    } else if (service[0]) {
+                        db.run(sqlInsert, [profile[0].userID, name, description, serviceName, JSON.stringify(params), widgetID], (err) => {
+                            if (err) { 
+                                throw err; 
+                            }
+                            if (service[0].widgetsName) {
+                                service[0].widgetsName[service[0].widgetsName.length] = widgetID;
+                            } else {
+                                service[0].widgetsName = [widgetID];
+                            }
+                            db.run(serviceInsert, [[JSON.stringify(service[0].widgetsName)], serviceName, profile[0].userID], (err) => {if (err) { throw err; }});
+                            res.status(201).send({message: 'Widget set successfully'});
+                        });
+                    } else {
+                        res.status(404).send({message: 'Service not found'});
+                    }
+                })
             });
         } else {
             res.status(403).send({ message: 'Token is not valid'});
