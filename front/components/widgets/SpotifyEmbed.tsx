e1d8a0da1820de50c3cd563b81a249a4dd94db28
Aj - Thomas <thomas.camlong@epitech.eu>
2021-12-13 11:44:48 +0100

    Add SpotifyTrackEmbed widget

--- front/components/widgets/SpotifyEmbed.tsx
+++ front/components/widgets/SpotifyEmbed.tsx
@@ -6,8 +6,8 @@ function SpotifyEmbed(props: any) {
 			<iframe
 			src={`https://open.spotify.com/embed/playlist/${props.playlistId}?utm_source=generator&theme=0`}
 			width="100%" 
-			height="380"
-			frameBorder="0"
+			height={props.height ? props.height : '400'}
+			frameBorder="1"
 			allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture">
 			</iframe>
 		</CardWrapper>
