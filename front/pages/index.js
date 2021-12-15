bb29d2c24cd82ee493410a5a48bf06637527b8d4
Aj - Thomas <thomas.camlong@epitech.eu>
2021-12-15 11:51:02 +0100

    Update index.js with mock values

--- front/pages/index.js
+++ front/pages/index.js
@@ -2,12 +2,42 @@ import ServicePannel from '../components/ServicePannel'
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { useCookies } from 'react-cookie'
-import { useToast } from "@chakra-ui/react";
+import {
+  Button, IconButton, useToast, Wrap, Menu,
+  MenuButton,
+  MenuList,
+  MenuItem
+} from "@chakra-ui/react";
+import SpotifyEmbed from '../components/widgets/SpotifyEmbed'
+import SpotifyTrackEmbed from '../components/widgets/SpotifyTrackEmbed';
+import { AddIcon, ChevronDownIcon } from '@chakra-ui/icons';
+import GithubStars from '../components/widgets/GithubStars';
+import GmailSender from '../components/widgets/GmailSender';
+import GmailUnread from '../components/widgets/GmailUnread';
+import SpotifyPlaylists from '../components/widgets/SpotifyPlaylists';
+import AddWidgetButton from '../components/AddWidgetButton';
+import spotifyProfile from '../components/widgets/SpotifyProfile';
+import githubProfile from '../components/widgets/GithubProfile';
+import YoutubeProfile from '../components/widgets/YoutubeProfile';
 
 export default function Home() {
 
+  const widgets = [
+    <SpotifyTrackEmbed songId="6eHKoMFFoMJ4cWwPscl382" />,
+    <SpotifyTrackEmbed songId="6Lvw6pbcXA4ugNMRQFlUaR"/>,
+    <SpotifyEmbed playlistId="37i9dQZF1DXcBWIGoYBM5M" />,
+    <SpotifyPlaylists />,
+    <GithubStars repoUrl="ajnart/mynetflix" />,
+    <spotifyProfile/>,
+    <githubProfile/>,
+    <YoutubeProfile/>,
+    <GmailSender />,
+    <GmailUnread />
+  ]
+
   const router = useRouter()
   const toast = useToast()
+  const [widets, setWidgets] = useState(widgets)
   const [service, setService] = useState("");
   const [cookies, setCookie] = useCookies(['name']);
   console.log(process.env.GOOGLE_ID)
@@ -24,8 +54,14 @@ export default function Home() {
   }, []);
 
   return !cookies["user"] ? <div></div> : (
-    <div>
+    <>
       <ServicePannel service={service} setService={setService} />
-    </div>
+      <Wrap justify={'center'} spacing={5}>
+        {widgets.map((widget, index) => {
+          return <div key={index}>{widget}</div>
+        })}
+      </Wrap>
+      <AddWidgetButton/>
+    </>
   )
 }
